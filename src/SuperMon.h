0b820c3b8682fcc0f1a7c4e8adaa1aee9f899663
Johannes <{ID}+{username}@users.noreply.github.com>
2022-11-01 19:25:29 +0100

    fix: script not returning acutal data

--- src/SuperMon.h
+++ src/SuperMon.h
@@ -1,16 +1,15 @@
 const char PAGE_MAIN[] PROGMEM = R"=====(
 
+
 <!DOCTYPE html>
 <html lang="en" class="js-focus-visible">
   <title>Tacho / Odometer</title>
-
   <style>
     html {
       padding: 0;
       height: 100%;
       width: 100%;
     }
-
     body,
     .outer {
       height: 100%;
@@ -30,7 +29,6 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
     .btn {
       background-color: #000000;
       border: none;
-
       color: white;
       padding: 10px 20px;
       text-align: center;
@@ -40,7 +38,6 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
       margin: 4px 2px;
       cursor: pointer;
     }
-
     .category {
       font-family: "Verdana", "Arial", sans-serif;
       font-weight: bold;
@@ -58,7 +55,6 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
     .valueBig {
       font-family: "Verdana", "Arial", sans-serif;
       vertical-align: top;
-
       font-weight: bold;
       font-size: 20vmin;
       padding: 0px 0px 0px 0px;
@@ -66,10 +62,8 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
       color: #000000;
     }
   </style>
-
   <!-- 
     ids: 
-
     distA
     distB
     totalDist
@@ -78,7 +72,6 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
     dailyDist
     speed
    -->
-
   <body style="background-color: #efefef" onload="process()">
     <!-- div container fullscreen -->
     <div class="outer">
@@ -183,10 +176,11 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
       </div>
     </div>
   </body>
-
   <script type="text/javascript">
-    const xmlHttp = createXmlHttpObject();
+    // global variable visible to all java functions
+    var xmlHttp = createXmlHttpObject();
 
+    // function to create XML object
     function createXmlHttpObject() {
       if (window.XMLHttpRequest) {
         xmlHttp = new XMLHttpRequest();
@@ -196,9 +190,18 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
       return xmlHttp;
     }
 
+    // function to handle button press from HTML code above
+    // and send a processing string back to server
+    // this processing string is use in the .on method
     function ButtonResetDistA() {
-      const xhttp = new XMLHttpRequest();
+      var xhttp = new XMLHttpRequest();
       var message;
+      // if you want to handle an immediate reply (like status from the ESP
+      // handling of the button press use this code
+      // since this button status from the ESP is in the main XML code
+      // we don't need this
+      // remember that if you want immediate processing feedbac you must send it
+      // in the ESP handling function and here
       /*
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -212,8 +215,11 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
       xhttp.send();
     }
 
+    // function to handle button press from HTML code above
+    // and send a processing string back to server
+    // this processing string is use in the .on method
     function ButtonResetDistB() {
-      const xhttp = new XMLHttpRequest();
+      var xhttp = new XMLHttpRequest();
       /*
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -225,69 +231,80 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
       xhttp.send();
     }
 
+
+    // function to handle the response from the ESP
     function response() {
       var message;
       var xmlResponse;
       var xmldoc;
-      const dt = new Date();
+      var dt = new Date();
+
 
       xmlResponse = xmlHttp.responseXML;
-      if (xmlResponse) {
-        // get host date and time
-        document.getElementById("time").innerText = dt.toLocaleTimeString();
-        document.getElementById("date").innerText = dt.toLocaleDateString();
+if(xmlResponse){
+      // console.log("xmlResponse", xmlResponse)
 
-        xmldoc = xmlResponse.getElementsByTagName("SPEED");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("speed").innerText = message;
-        }
-        xmldoc = xmlResponse.getElementsByTagName("DISTA");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("distA").innerText = message;
-        }
-        xmldoc = xmlResponse.getElementsByTagName("DISTB");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("distB").innerText = message;
-        }
-        xmldoc = xmlResponse.getElementsByTagName("TOTALDIST");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("totalDist").innerText = message;
-        }
-        xmldoc = xmlResponse.getElementsByTagName("TOTALTIME");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("totalTime").innerText = message;
-        }
-        xmldoc = xmlResponse.getElementsByTagName("DAILYTIME");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("dailyTime").innerText = message;
-        }
+      // get host date and time
+      document.getElementById("time").innerText = dt.toLocaleTimeString();
+      document.getElementById("date").innerText = dt.toLocaleDateString();
 
-        xmldoc = xmlResponse.getElementsByTagName("DAILYDIST");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("dailyDist").innerText = message;
-        }
-        xmldoc = xmlResponse.getElementsByTagName("POSITION");
-        if (xmldoc) {
-          message = xmldoc[0].firstChild.nodeValue;
-          document.getElementById("position").innerText = message;
-        }
-      }
+
+      xmldoc = xmlResponse.getElementsByTagName("SPEED");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("speed").innerText = message;
+ }
+      xmldoc = xmlResponse.getElementsByTagName("DISTA");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("distA").innerText = message;
+ }
+      xmldoc = xmlResponse.getElementsByTagName("DISTB");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("distB").innerText = message;
+ }
+      xmldoc = xmlResponse.getElementsByTagName("TOTALDIST");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("totalDist").innerText = message;
+ }
+      xmldoc = xmlResponse.getElementsByTagName("TOTALTIME");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("totalTime").innerText = message;
+ }
+      xmldoc = xmlResponse.getElementsByTagName("DAILYTIME");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("dailyTime").innerText = message;
+}
+ 
+      xmldoc = xmlResponse.getElementsByTagName("DAILYDIST");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("dailyDist").innerText = message;
+ }
+      xmldoc = xmlResponse.getElementsByTagName("POSITION");
+      if(xmldoc){
+      message = xmldoc[0].firstChild.nodeValue;
+      document.getElementById("position").innerText = message;
+ }}
     }
 
+    // general processing code for the web page to ask for an XML steam
+    // this is actually why you need to keep sending data to the page to
+    // force this call with stuff like this
     // server.on("/xml", SendXML);
+    // otherwise the page will not request XML from the ESP, and updates will not happen
     function process() {
       if (xmlHttp.readyState == 0 || xmlHttp.readyState == 4) {
         xmlHttp.open("PUT", "xml", true);
         xmlHttp.onreadystatechange = response;
         xmlHttp.send(null);
       }
+      // you may have to play with this value, big pages need more porcessing time, and hence
+      // a longer timeout
       setTimeout("process()", 100);
     }
   </script>
